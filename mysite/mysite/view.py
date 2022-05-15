27e1ed58afb814704678c26571d1ed08aed46d18
Nikhil <nikhilsaraswat644@gmail.com>
2022-05-15 04:31:23 +0100

    added template

--- mysite/mysite/view.py
+++ mysite/mysite/view.py
@@ -1,9 +1,11 @@
 # This file has been created by me. - Nikhil
 # HttpResponse required by browser
 from django.http import HttpResponse
+from django.shortcuts import render
 #Default argument request is required
 def index(request):
-    return HttpResponse("hello") # Can pass text, html
+    return render(request, 'index.html')
+    # return HttpResponse("hello") # Can pass text, html
 
 def removepunc(request):
     return HttpResponse("remove punc")
@@ -15,7 +17,7 @@ def newlineremove(request):
     return HttpResponse("about us")
 
 def spaceremove(request):
-    return HttpResponse("new line remove")
+    return HttpResponse("new line remove <a href='/'>back</a>")
 
 def charcount(request):
     return HttpResponse("char count")
